2b9bf26ac548116f3f0686d565cc39c3f2b97eb1
kate <grclnd@gmail.com>
2016-09-17 22:13:55 +0800

    resolves #11 - header change after log in.

--- www/login.php
+++ www/login.php
@@ -6,29 +6,33 @@
 <body>
 
   <?php
-    include 'include/header.php';
+
     $user_name="";
     $password="";
+    $msg="";
 
     if(isset($_POST["submit_login"])){
       $user_name=$_POST["user_name"];
       $password=$_POST["password"];
 
       if($UT->user_log_in($user_name,$password,$db_handler)){
-        echo "<br>Welcome ".$_SESSION["user_object"]->user_name.", you have successfully logged in.";
+        $msg= "Welcome ".$_SESSION["user_object"]->user_name.", you have successfully logged in.";
       } else {
-        echo "<br>Failed to log in, wrong user/password combination, or user not activated.";
+        $msg= "Failed to log in, wrong user/password combination, or user not activated.";
       }
     }
+    include 'include/header.php';
 
   ?>
   <br><a href="index.php">Home</a>
 
   <form action="login.php" method="post">
     User Name: <input type="text" name="user_name" value="<?php echo $user_name ?>"/><br>
-    Password: <input type="password" name="password" value="<?php echo $password ?>"/><br>
+    Password: <input type="password" name="password" value=""/><br>
     <input type="submit" value="Login" name="submit_login" />
   </form>
+
+  <?php echo $msg; ?>
 
 </body>
 
